d6f5cca85fc5c96bf1c7f1dab423860428e5356d
Samer Shaban <samershaban@Samers-MBP.fios-router.home>
2019-11-25 08:07:07 -0500

    updated Cryptr.java

--- src/Cryptr.java
+++ src/Cryptr.java
@@ -1,3 +1,29 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
 /*
  *               Cryptr
  *
@@ -25,7 +51,21 @@ public class Cryptr {
 	 */
 	static void generateKey(String secKeyFile) throws Exception{
 
-		/*   FILL HERE   */
+		// Generate AES Key, 128 bits
+		KeyGenerator kgen = KeyGenerator.getInstance("AES");
+		kgen.init(128);
+		SecretKey skey = kgen.generateKey();
+
+		// Output key to file
+		FileOutputStream outStream = null;
+		try {
+			outStream = new FileOutputStream(secKeyFile);
+			byte[] keyBytes = skey.getEncoded();
+			outStream.write(keyBytes);
+			outStream.close();
+		} catch (Exception ex) {
+			throw new Exception("Error in outputting key to file. " + ex.getMessage(), ex);
+		}
 
 	}
 
@@ -42,7 +82,59 @@ public class Cryptr {
 	 */
 	static void encryptFile(String originalFile, String secKeyFile, String encryptedFile) {
 
-		/*   FILL HERE   */
+		// === Import secret Key from file
+		byte[] keyBytes = null;
+		SecretKeySpec sKey = null;
+		try {
+			keyBytes = Files.readAllBytes(Paths.get(secKeyFile));
+			sKey = new SecretKeySpec(keyBytes, "AES");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} // IOException
+
+		// === Initialization Vector
+		final SecureRandom srandom = new SecureRandom();
+		byte[] iv = new byte[128 / 8];
+		srandom.nextBytes(iv);
+		IvParameterSpec ivspec = new IvParameterSpec(iv);
+
+		// === Cipher Creation
+		String cipherType = "AES/CBC/PKCS5Padding";
+		Cipher ciph = null;
+		try {
+			ciph = Cipher.getInstance(cipherType); // e1 error
+			ciph.init(Cipher.ENCRYPT_MODE, sKey, ivspec); // e2 error
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException e1) {
+			e1.printStackTrace();
+		} catch (InvalidKeyException | InvalidAlgorithmParameterException e2) {
+			e2.printStackTrace();
+		}
+
+		// === Open original file => Encrypt => Output
+		FileInputStream inOriStream = null;
+		FileOutputStream outEncryptStream = null;
+		try {
+			// open files
+			inOriStream = new FileInputStream(originalFile); 
+			outEncryptStream = new FileOutputStream(encryptedFile);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			outEncryptStream.write(iv);
+			byte[] ibuf = new byte[1024];
+			int len;
+			while ((len = inOriStream.read(ibuf)) != -1) {
+				byte[] obuf = ciph.update(ibuf, 0, len);
+				if (obuf != null)
+					outEncryptStream.write(obuf);
+			}
+			byte[] obuf = ciph.doFinal();
+			if (obuf != null)
+				outEncryptStream.write(obuf);
+		} catch (IOException | IllegalBlockSizeException | BadPaddingException e) {
+			e.printStackTrace();
+		}
 
 	}
 
@@ -59,7 +151,64 @@ public class Cryptr {
 	 */
 	static void decryptFile(String encryptedFile, String secKeyFile, String outputFile) {
 
-		/*   FILL HERE   */
+		// Read Initialization Vector + Extract IV
+		FileInputStream inIVStream = null;
+		byte[] iv = new byte[128 / 8];
+		try {
+			inIVStream = new FileInputStream(encryptedFile);
+			inIVStream.read(iv);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// IVSpec + loading secret key
+		IvParameterSpec ivspec = new IvParameterSpec(iv);
+		byte[] keyBytes;
+		SecretKeySpec sKey = null;
+		try {
+			keyBytes = Files.readAllBytes(Paths.get(secKeyFile));
+			sKey = new SecretKeySpec(keyBytes, "AES");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// Initialize Cipher to Decryption Mode
+		String cipherType = "AES/CBC/PKCS5Padding";
+		Cipher ciph = null;
+		try {
+			ciph = Cipher.getInstance(cipherType);
+			ciph.init(Cipher.DECRYPT_MODE, sKey, ivspec);
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException e1) {
+			e1.printStackTrace();
+		} catch (InvalidKeyException | InvalidAlgorithmParameterException e2) {
+			e2.printStackTrace();
+		}
+
+		//  Decrypt file
+		FileInputStream input = null;
+		FileOutputStream outDecryptStream = null;
+		try {
+			input = inIVStream;
+			outDecryptStream = new FileOutputStream(outputFile);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		byte[] ibuf = new byte[1024];
+		int len;
+		try {
+			while ((len = input.read(ibuf)) != -1) {
+				byte[] obuf = ciph.update(ibuf, 0, len);
+				if (obuf != null)
+					outDecryptStream.write(obuf);
+			}
+			byte[] obuf = ciph.doFinal();
+			if (obuf != null)
+				outDecryptStream.write(obuf);
+		} catch (IllegalBlockSizeException | BadPaddingException | IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
@@ -74,7 +223,44 @@ public class Cryptr {
 	 */
 	static void encryptKey(String secKeyFile, String pubKeyFile, String encKeyFile) {
 
-		/*   FILL HERE   */
+
+		// Load RSA Key
+		PublicKey publicKey = null;
+		try {
+			byte[] RSAbytes = Files.readAllBytes(Paths.get(pubKeyFile));
+			X509EncodedKeySpec ks = new X509EncodedKeySpec(RSAbytes);
+			KeyFactory keyfac = KeyFactory.getInstance("RSA");
+			publicKey = keyfac.generatePublic(ks);
+		} catch (NoSuchAlgorithmException | InvalidKeySpecException | IOException e) {
+			e.printStackTrace();
+		}
+
+		// load AES secret key
+		SecretKeySpec skey = null;
+		try {
+			byte[] AESkeybytes = Files.readAllBytes(Paths.get(secKeyFile));
+			skey = new SecretKeySpec(AESkeybytes, "AES");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// Initialize Cipher for encryption
+		Cipher cipher = null;
+		try {
+			cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+			cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+		} catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException e) {
+			e.printStackTrace();
+		}
+
+		// Output
+		try {
+			FileOutputStream out = new FileOutputStream(encKeyFile);
+			byte[] b = cipher.doFinal(skey.getEncoded());
+			out.write(b);
+		} catch (IllegalBlockSizeException | BadPaddingException | IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
@@ -89,7 +275,43 @@ public class Cryptr {
 	 */
 	static void decryptKey(String encKeyFile, String privKeyFile, String secKeyFile) {
 
-		/*   FILL HERE   */
+		// load AES from encKey
+		SecretKeySpec skey = null;
+		try {
+			byte[] keyb = Files.readAllBytes(Paths.get(encKeyFile));
+			skey = new SecretKeySpec(keyb, "AES");
+		} catch (IOException e3) {
+			e3.printStackTrace();
+		}
+
+		// Private RSA 
+		PrivateKey pvt = null;
+		try {
+			byte[] bytes = Files.readAllBytes(Paths.get(privKeyFile));
+			PKCS8EncodedKeySpec ks = new PKCS8EncodedKeySpec(bytes);
+			KeyFactory kf = KeyFactory.getInstance("RSA");
+			pvt = kf.generatePrivate(ks);
+		} catch (NoSuchAlgorithmException | InvalidKeySpecException | IOException e2) {
+			e2.printStackTrace();
+		}
+
+		// Initialize Cipher for decryption
+		Cipher cipher = null;
+		try {
+			cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+			cipher.init(Cipher.DECRYPT_MODE, pvt);
+		} catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException e1) {
+			e1.printStackTrace();
+		}
+
+		// Output
+		try {
+			FileOutputStream out = new FileOutputStream(secKeyFile);
+			byte[] b = cipher.doFinal(skey.getEncoded());
+			out.write(b);
+		} catch (IllegalBlockSizeException | BadPaddingException | IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
